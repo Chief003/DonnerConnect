c2e576f8e3731fa87e603b739db2402411b8283e
Chief003 <debigchief03@gmail.com>
2024-07-14 21:53:52 +0300

    Update Home1.js

---
+++ User-Auth/src/components/School/Home1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SchoolPage from "./SchoolPage";
+import  Header  from "../Header";
+const Home1 = () =>{
+return(
+    <>
+    <Header/>
+    <SchoolPage/>
+    </>
+)
+};
+export default Home1;
